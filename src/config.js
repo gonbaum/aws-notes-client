ca327d663e63e1957f6b9fdd0256b43c1efdb399
gonbaum <nouvellegon@gmail.com>
2020-03-22 19:38:36 +0100

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-1",
+      BUCKET: "notes-app-uploads-gon"
+    },
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://ho4wdov9ql.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_rbZEawHqs",
+      APP_CLIENT_ID: "2mpeitgacpedij5na1b2f38ib1",
+      IDENTITY_POOL_ID: "eu-west-1:a4d69e04-e098-4dc5-a219-125152056f23"
+    }
+  };
